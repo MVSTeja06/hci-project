79588aab6a4b16412d641b0ab4dd2d25a85fd009
madhubalamantena94 <madhubalamantena94@gmail.com>
2023-04-20 23:17:03 -0400

    init cmmit

---
+++ src/menu-items/index.js
@@ -0,0 +1,14 @@
+import dashboard from './dashboard';
+import other from './other';
+import pages from './pages';
+import utilities from './utilities';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+    items: [dashboard, pages, utilities, other]
+
+    // items: [dashboard, utilities]
+};
+
+export default menuItems;
